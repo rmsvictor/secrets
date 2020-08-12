da8f59597e8b1d91887cf3c9a8e9b5bec37809bc
Victor Hugo Ramos Ramirez <VictorHR@digilab.com.mx>
2020-08-12 12:35:18 -0500

    Level 1 security, username and password check ok

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model('User', userSchema);
+
+
+
 app.get('/', function(req,res){
   res.render('home.ejs');
 });
@@ -21,7 +30,42 @@ app.get('/login', function(req,res){
 
 app.get('/register', function(req,res){
   res.render('register.ejs');
+  });
+
+
+app.post('/register', function(req,res){
+ const newUser = new User({
+   email: req.body.username,
+   password: req.body.password
+ });
+
+ newUser.save(function(err){
+   if(err){
+     console.log(err);
+   } else {
+     res.render('secrets.ejs');
+   }
+ });
 });
+
+app.post('/login', function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render('secrets');
+        }
+      }
+    }
+  });
+});
+
+
 
 
 app.listen(3000,function(){
